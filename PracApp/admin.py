ae19958879ced78d73d95c5a2dba76cbdbc8d521
RaoMubashir5 <mubashir.qadeer@arbisoft.com>
2024-08-13 00:08:03 +0500

    added Person and Student models, and createdsuper user

--- PracApp/admin.py
+++ PracApp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from PracApp.models import *
+
+class admin_customize(admin.ModelAdmin):
+    """"these are the customized functionalities of the django admin site """
+    
+    # all the instances of the Student model with these Fields
+    list_display=('first_name','last_name','date_of_birth',)  
+
+    # Added filters for these two fields
+    list_filter = ('date_of_birth','first_name',)    
+
+    #Search option also implemented and Search would be on the basis of "first_name"          
+    search_fields=('first_name',)                             
+
+    #apply order listing of the records
+    ordering=('-first_name',)                     
+    
+#regitering the models with the admin site
+admin.site.register(Student,admin_customize)                   
--- PracApp/models.py
+++ PracApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50,verbose_name="First Name")    
+    last_name = models.CharField(max_length=50,verbose_name="Last Name")
+    date_of_birth = models.DateField(verbose_name="Date of Birth")
+    #if we do not use verbose it will automatically capitilized the first alpabet, and remove the '_' underscore.to make it easy readable
+
+    class Meta:
+        """"meta class is class that is used to perform many functionalities like,
+            ordering the instances on based of any field,adding permissions to the field,
+            making any class Abstract(this is just virtual class Would not be in database, but the class that inherits it)."""
+        
+        abstract=True
+        ordering=['first_name']
+
+class Student(Person):
+    """This class inheriting the Abstract class and have all that abstract class functionalities ,
+       This make the less redundant code, and follow the [Do not repeat youself principle of coding]"""
+    
+    roll_number=models.IntegerField(verbose_name="Roll Number")
+
+    class Meta:
+        verbose_name="Registered Student"
