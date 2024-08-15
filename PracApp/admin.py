55d22cefd50d9d526c3e7e1975dab97e03bb25e4
RaoMubashir5 <mubashir.qadeer@arbisoft.com>
2024-08-15 14:32:37 +0500

    This commit is about the creation of more table like teacher,course and through relationship understanding.It is about the models relations and like many to one and many to many.It is also have the admin customization that are retalted to the filter and display and other field ordering and edit options.This coomit is to add the database into the .gitignore file , to obtain data integrity.

--- PracApp/admin.py
+++ PracApp/admin.py
@@ -1,22 +1,79 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
 
 # Register your models here.
 from PracApp.models import *
 
+class customize_filter_for_students_under_eache_lead(admin.SimpleListFilter):
+    """This class is customize the filter options in the filter of the admin site.
+       We would override the lookup(is to define the options name, and the url send back to the queryset function).
+       and queryset(which filters the data accroding to the lookup function names/options in actual ) functions in this regard."""
+    
+    title='Student Under Each Lead'
+    parameter_name='students_under_lead'
+
+    def lookups(self, request, model_admin) -> list[tuple[Any, str]]:
+
+        group_leads=Student.objects.filter(group_lead__isnull=True)
+        options=[
+                 (g_lead.id,g_lead.first_name) for g_lead in group_leads]  
+        """the first value of the tuple would be pass to queryset:
+           self.value()=first Parameter"""
+        new_filter=('group_leads','Group Leads')
+       
+        # index 0 element si stored to be appended again at the end
+        return options.append(new_filter)
+    def queryset(self, request: Any, queryset: QuerySet[Any]) -> QuerySet[Any] | None:
+
+        # check what option is clicked that is defined in the lookups
+        if self.value():
+            print(type(self.value()),self.value())
+            if self.value() == 'group_leads':
+                # it return the queryset with only that student Table records that has group_lead id only 
+                return queryset.filter(group_lead__isnull=True) 
+            #it will filter the records of the student table with only this group_lead that's id id in self.value()
+            return  queryset.filter(group_lead_id=self.value())                  
 class admin_customize(admin.ModelAdmin):
     """"these are the customized functionalities of the django admin site """
     
     # all the instances of the Student model with these Fields
-    list_display=('first_name','last_name','date_of_birth',)  
+    list_display=('first_name','last_name','date_of_birth','group_lead','teacher',)  
 
     # Added filters for these two fields
-    list_filter = ('date_of_birth','first_name',)    
+    list_filter = ('date_of_birth','first_name',)   
+
+    list_filter=(customize_filter_for_students_under_eache_lead,'teacher',) 
 
     #Search option also implemented and Search would be on the basis of "first_name"          
     search_fields=('first_name',)                             
 
     #apply order listing of the records
-    ordering=('-first_name',)                     
+    ordering=('-first_name',)                
+
+    #add another button
+    list_editable=("group_lead",'teacher')     
+   
+class cutomize_Teacher_model(admin.ModelAdmin):
+    list_display=('name',)
     
+
+class customize_course_model(admin.ModelAdmin):
+   
+    list_display=('course_name',)
+ 
+class customize_enroll_model(admin.ModelAdmin):
+   
+    list_display=('student','course','enrollment_date')
+    list_filter=('student','course','enrollment_date')
+    # def get_students(self, obj):
+    #     # Filter students by the course
+    #     # Return student names, each on a new line
+    #     return ",".join([str(student) for student in obj.student.all()])
+    # get_students.short_description = 'Enrolled Students'
+
 #regitering the models with the admin site
-admin.site.register(Student,admin_customize)                   
+admin.site.register(Student,admin_customize)
+admin.site.register(teacher,cutomize_Teacher_model)   
+admin.site.register(course,customize_course_model)                   
+admin.site.register(Enrollment,customize_enroll_model) 
--- PracApp/models.py
+++ PracApp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.db.models import Q
 # Create your models here.
 
 class Person(models.Model):
@@ -22,5 +22,48 @@ class Student(Person):
     
     roll_number=models.IntegerField(verbose_name="Roll Number")
 
+    #if a alumni Category got deleted than the students with that Tag would't be deteled but the :Alumni field would be set to null instead.
+    group_lead=models.ForeignKey('self',on_delete=models.SET_NULL, related_name="group_students",null=True,blank=True,verbose_name="Group Head") 
+    teacher=models.ForeignKey('teacher',on_delete=models.CASCADE,verbose_name="Teachers",null=True,blank=True,related_name="teachers")  #,verbose_name="Group Leads"
+    #course=models.ForeignKey('course',on_delete=models.SET_NULL,related_name="Course",null=True,blank=True)
+    
     class Meta:
         verbose_name="Registered Student"
+    def __str__(self) -> str:
+        return self.first_name
+    
+
+class teacher(models.Model):
+    """there would be the many to retation between the students and teacher"""
+    name=models.CharField(max_length=15,verbose_name="Teachers")
+    def __str__(self) -> str:
+        return self.name
+    
+
+class course(models.Model):
+    course_name=models.CharField(max_length=30)
+    student=models.ManyToManyField(Student,through='Enrollment',)
+
+    def __str__(self) -> str:
+        return self.course_name
+    
+    # they are allowed in django in many to many relation ,
+    # we have defoon a through table to do so
+
+    # class Meta:
+    #     unique_together = ('student', 'course_name',) 
+
+   
+
+# Enrollment model as the Through model;;it is now custom django intermediatory table for many to many relation,
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE,verbose_name="Enrolled Students")
+    course = models.ForeignKey(course, on_delete=models.CASCADE)
+    enrollment_date=models.DateField(verbose_name="Date of Enrollment")
+
+    class Meta:
+        unique_together = ('student', 'course')
+        verbose_name = "Enrollment"
+
+    def __str__(self):
+        return f"{self.student} in {self.course}"
