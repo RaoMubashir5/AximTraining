a63ec33c6ccf404255d2619dbf505523d7942004
RaoMubashir5 <mubashir.qadeer@arbisoft.com>
2024-08-31 03:02:13 +0500

    To apply functionality for each user different and add the functionality manualy sessionSuthentication and other djangoModelPermissions

--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from rest_framework.authentication import BasicAuthentication,SessionAuthentication,TokenAuthentication
 
 from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly
+from rest_framework.permissions import DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
 #Generic API views
 
 from django.contrib.auth import logout
@@ -20,9 +21,16 @@ class UserViewSet(viewsets.ModelViewSet):
 
     #overriding the global authenticationa and permission to allow any.
     authentication_classes=[SessionAuthentication]
-    permission_classes=[IsAuthenticatedOrReadOnly]
+    #permission_classes=[IsAdminUser]
+      #permission_classes=[IsAuthenticatedOrReadOnly]
+      #permission_classes=[IsAuthenticated]
+    #django permissions that are only aplicable after authorization, and you manually assign the functionality for Use that it can perform.]
+    #permission_classes=[DjangoModelPermissions]  #super user have all the permissions by default
 
-  
+    #anon readonly is variation of the django model permissions as it alow user to view only functionlity without authenticating. but othe ,
+    #would be added manualy
+    permission_classes=[DjangoModelPermissionsOrAnonReadOnly] 
+
    
 
    
