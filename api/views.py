9fe9a071f767788e570a2d154c19cd53a871cb2a
RaoMubashir5 <mubashir.qadeer@arbisoft.com>
2024-08-30 13:42:57 +0500

    Added doc_string ReadOnly ViewSets are implemented  and dynamic routing

--- api/views.py
+++ api/views.py
@@ -9,8 +9,20 @@ from rest_framework import status
 from rest_framework import viewsets
 #it is for model creation
 # Read only model viewsets
+
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
 
+    """class UserViewSet(viewsets.ModelViewSet): these are the model view sets that are that inherits the genericApiView and ViewSets 
+    ReadOnlyModelViewSet is to  only listing and retreiving api not any other actions can be perform by the client.
+    and we can perform all operations and there routing would also be done dynaicaly based on the request method , there are the attributes
+    that tells us about the which sort of the action is going to be performed like (list,retreive(detail),update,partial_update,destroy)
+    suffix are like they tell us the opration is performed on instance or list of  instences(list). basename is useful to creating the reverse URLs
+    like if the basename is = 'toy' than the reverse url for each fuction would be : toy-list,toy-detail. 
+    there is also  a base Url that is the first paramater of the router's registration function in url.py(it is the base of every url endpoint) like 
+    if you whant to see the all users url would be:http://127.0.0.1:8000/user/  ; 'user' is the base url here and for detail view your url would be
+    like this :http://127.0.0.1:8000/user/7/   ; again base would remain same for all actions."""
+
+    
     serializer_class=UserSerializer
     # queryset=User.objects.all()  it is attribute btu we can use the function as well to customize 
 
